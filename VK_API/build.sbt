c44063fb33e80b9226838c19be4d4356de75a483
Kirill Yuldashev <kirill.yuldashev@mail.ru>
2018-04-10 23:06:32 +0500

    vk api wrapper fix

--- VK_API/build.sbt
+++ VK_API/build.sbt
@@ -4,4 +4,4 @@ version := "0.1"
 
 scalaVersion := "2.12.5"
 
-libraryDependencies += "org.json4s" %% "json4s-jackson" % "3.2.11"
+libraryDependencies += "org.scala-lang.modules" % "scala-xml_2.12" % "1.0.6"
---
+++ VK_API/src/main/scala/ApiWrapper.scala
@@ -0,0 +1,15 @@
+import scala.util.Try
+import scala.xml._
+
+
+trait ApiWrapper {
+  private def mkUrl(method_name: String, v: String, parameters: Seq[String]): String = {
+    s"https://api.vk.com/method/$method_name.xml?${parameters.mkString("&")}&v=$v"
+  }
+
+  def mkQuery(method_name: String, v: String, parameters: Seq[String]): Try[Elem] = {
+    Try[Elem] {
+      XML.load(mkUrl(method_name, v, parameters))
+    }
+  }
+}
