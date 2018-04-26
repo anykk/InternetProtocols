6e69c946068f8249934e19ba724527899abaef6f
Kirill Yuldashev <kirill.yuldashev@mail.ru>
2018-04-26 15:07:59 +0500

    fix cache

--- DNS/main.py
+++ DNS/main.py
@@ -25,7 +25,15 @@ def store_cache(cache):
 
 def main():
     def add_entry(p):
-        cache[(p.Question[0].QNAME, p.Question[0].QTYPE)] = (p, time() + p.Answer[0].TTL)
+        counts = enumerate([p.Header.ANCOUNT, p.Header.NSCOUNT, p.Header.ARCOUNT], start=2)
+        for i, count in counts:
+            for j in range(count):
+                if (p[i][j].NAME, p[i][j].TYPE) not in cache:
+                    cache[(p[i][j].NAME, p[i][j].TYPE)] = (
+                        DNSPacket(Header(ID=-1, QR=1, Opcode=0, AA=0, TC=0, RD=1, RA=0, Z=0,
+                                         RCODE=0, QDCOUNT=1, ANCOUNT=len(p[i]), NSCOUNT=0, ARCOUNT=0),
+                                  Question=tuple([Question(QNAME=p[i][j].NAME, QTYPE=p[i][j].TYPE, QCLASS=p[i][j].CLASS)])
+                                  , Answer=p[i], Authority=tuple(), Additional=tuple()), time() + p[i][j].TTL)
 
     def get_entry(p):
         e = cache.get((p.Question[0].QNAME, p.Question[0].QTYPE), None)
@@ -40,48 +48,47 @@ def main():
         for k in expired:
             del cache[k]
 
-    try:
-        server_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        server_sock.bind(('localhost', 53))
+    cache = {}
 
-        query_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        query_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_sock.bind(('localhost', 53))
 
-        cache = load_cache()
+    query_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    query_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-        while True:
-            query, addr = server_sock.recvfrom(1024)
-            packet = packet_frombytes(bitstring.Bits(query))
-            pprint(f'Request from: {addr}')
-            pprint(packet._asdict(), indent=2)
+    cache = load_cache()
 
-            entry = get_entry(packet)
-            if not entry:
-                print('Cache MISS')
+    while True:
+        query, addr = server_sock.recvfrom(1024)
+        packet = packet_frombytes(bitstring.Bits(query))
+        pprint(f'Request from: {addr}')
+        pprint(packet._asdict(), indent=2)
 
-                query_sock.connect(('8.8.8.8', 53))
-                query_sock.sendall(packet_tobytes(packet))
-                answer = query_sock.recv(1024)
+        entry = get_entry(packet)
+        if not entry:
+            print('Cache MISS')
 
-                anspacket = packet_frombytes(bitstring.Bits(answer))
+            query_sock.connect(('212.193.163.6', 53))
+            query_sock.sendall(packet_tobytes(packet))
+            answer = query_sock.recv(1024)
 
-                add_entry(anspacket)
+            anspacket = packet_frombytes(bitstring.Bits(answer))
 
-                pprint(f'Answer for: {addr}')
-                pprint(anspacket._asdict(), indent=2)
-            else:
-                print('Cache HIT')
+            add_entry(anspacket)
+            store_cache(cache)
 
-                header = Header(packet.Header.ID, *entry.Header[1:])
-                anspacket = DNSPacket(header, *entry[1:])
+            pprint(f'Answer for: {addr}')
+            pprint(anspacket._asdict(), indent=2)
+        else:
+            print('Cache HIT')
 
-                pprint(anspacket._asdict(), indent=2)
+            header = Header(packet.Header.ID, *entry.Header[1:])
+            anspacket = DNSPacket(header, *entry[1:])
 
-            server_sock.sendto(packet_tobytes(anspacket), addr)
-    except (KeyboardInterrupt, Exception):
-        store_cache(cache)
-        print('Bye.')
+            pprint(anspacket._asdict(), indent=2)
+
+        server_sock.sendto(packet_tobytes(anspacket), addr)
 
 
 if __name__ == '__main__':
